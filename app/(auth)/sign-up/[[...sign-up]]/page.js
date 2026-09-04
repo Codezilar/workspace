d44d83dc41dcd64fd828bf3af3e33186bfa09ff1
Goodness Christopher <zoeehimen@gmail.com>
2026-09-04 09:42:55 +0100

    Doing

--- app/(auth)/sign-up/[[...sign-up]]/page.js
+++ app/(auth)/sign-up/[[...sign-up]]/page.js
@@ -1,1 +1,1 @@
-import {SignUp} from '@clerk/nextjs'; export default function Page(){return <main className="grid min-h-screen place-items-center bg-ink p-5"><SignUp forceRedirectUrl="/payment"/></main>}
+import {SignUp} from '@clerk/nextjs'; export default function Page(){return <main className="grid min-h-screen place-items-center bg-ink p-5"><SignUp forceRedirectUrl="/dashboard"/></main>}
